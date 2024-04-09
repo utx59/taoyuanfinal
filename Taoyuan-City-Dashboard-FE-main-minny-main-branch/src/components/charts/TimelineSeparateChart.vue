cf572e3f7012b69d03d87680bfb6beeba35a9522
utx59 <utx0359@gmail.com>
2024-04-10 00:20:23 +0800

    taoyuan

---
+++ Taoyuan-City-Dashboard-FE-main-minny-main-branch/src/components/charts/TimelineSeparateChart.vue
@@ -0,0 +1,84 @@
+<!-- Developed by Taipei Urban Intelligence Center 2023 -->
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps(["chart_config", "activeChart", "series"]);
+
+const chartOptions = ref({
+	chart: {
+		toolbar: {
+			show: false,
+			tools: {
+				zoom: false,
+			},
+		},
+	},
+	colors: props.chart_config.color,
+	dataLabels: {
+		enabled: false,
+	},
+	grid: {
+		show: false,
+	},
+	legend: {
+		show: props.series.length > 1 ? true : false,
+	},
+	markers: {
+		hover: {
+			size: 5,
+		},
+		size: 3,
+		strokeWidth: 0,
+	},
+	stroke: {
+		colors: props.chart_config.color,
+		curve: "smooth",
+		show: true,
+		width: 2,
+	},
+	tooltip: {
+		custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+			return '<div class="chart-tooltip">' +
+				'<h6>' + w.globals.labels[dataPointIndex] + '</h6>' +
+				'<span>' + series[seriesIndex][dataPointIndex] + ` ${props.chart_config.unit}` + '</span>' +
+				'</div>';
+		},
+		followCursor: true,
+	xaxis: {
+		axisBorder: {
+			color: "#555",
+			height: "0.8",
+		},
+		axisTicks: {
+			show: false,
+		},
+		crosshairs: {
+			show: false,
+		},
+		tooltip: {
+			enabled: false,
+		},
+		type: "datetime",
+		title: {
+                text: "Month"
+              }
+	},
+}});
+
+function parseTime(time) {
+	return time.replace("T", " ").replace("+08:00", " ");
+}
+</script>
+
+<template>
+	<div v-if="activeChart === 'TimelineSeparateChart'">
+		<apexchart
+			width="100%"
+			height="260px"
+			type="line"
+			:options="chartOptions"
+			:series="series"
+		></apexchart>
+	</div>
+</template>
